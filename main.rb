dfe3b10e9bd69746c543e9fefbbcc133058f038d
Yauheni Shaikouski <evgenyshaikovskiy@gmail.com>
2022-10-09 18:16:23 +0300

    add all crud functionality

--- main.rb
+++ main.rb
@@ -8,7 +8,12 @@ require_relative 'utils'
 class Application
   def initialize
     @help_messages = ApplicationUtility.help_messages
-    @commands = ApplicationUtility.commands
+    @commands = {
+      'help' => method(:print_help), 'exit' => method(:exit),
+      'stats' => method(:stats), 'create' => method(:create),
+      'list' => method(:list), 'edit' => method(:edit),
+      'find' => method(:find), 'delete' => method(:delete)
+    }
     @is_running = true
   end
 
@@ -27,24 +32,6 @@ class Application
   end
 
   private
-
-  def parse_command
-    input = Readline.readline('> ', true)
-    command_input = input.split(' ', 2)
-    command = command_input.first
-
-    parameters = command_input.length == 2 ? command_input.last : ''
-
-    [command, parameters]
-  end
-
-  def execute_command(command, parameters)
-    if @commands.key?(command)
-      @commands[command].call(parameters)
-    else
-      print_help(command)
-    end
-  end
 
   def stats(_parameters)
     result = @con.exec 'SELECT COUNT(*) FROM users;'
@@ -102,15 +89,85 @@ class Application
 
   def list(_parameters)
     result = @con.exec 'SELECT * FROM users;'
-    string = ''
-    result.to_a.each do |instance|
-      string += '---' * 10
-      string += "\n"
-      instance.each { |key, value| string += "#{key}: #{value}\n" }
-    end
-    puts string
+    form_output(result)
   rescue PG::Error => e
     puts e.message
+  end
+
+  def edit(parameters)
+    where_clause = form_where_clause(parameters)
+    puts 'Found following records: '
+    find(parameters)
+    puts 'Set up properties for those records: '
+    editted = gets.chomp.to_s
+    set_clause = form_set_clause(editted)
+    @con.exec "UPDATE users #{set_clause} #{where_clause};"
+  rescue PG::Error => e
+    puts e.message
+  end
+
+  def find(parameters)
+    where_clause = form_where_clause(parameters)
+    result = @con.exec "SELECT * FROM users #{where_clause};"
+    form_output(result)
+  rescue PG::Error => e
+    puts e.message
+  end
+
+  def delete(parameters)
+    where_clause = form_where_clause(parameters)
+    @con.exec "DELETE FROM users #{where_clause}"
+  rescue PG::Error => e
+    puts e.message
+  end
+
+  # methods that could be moved to utility
+  def form_where_clause(parameters)
+    keys = ApplicationUtility.parse_selection_params(parameters)
+    form_query(keys, 'WHERE ', 'AND')
+  end
+
+  def form_set_clause(parameters)
+    keys = ApplicationUtility.parse_selection_params(parameters)
+    form_query(keys, 'SET ', ',')
+  end
+
+  def form_output(result)
+    string = ''
+    result.to_a.each do |instance|
+      string << '---' * 10
+      string << "\n"
+      instance.each { |key, value| string << "#{key}: #{value}\n" }
+    end
+    puts string
+  end
+
+  def form_query(keys, keyword, delimiter)
+    query = keyword
+    keys.each_pair do |key, value|
+      key = key.sub('-', '_') if key.include? '-'
+      query << "#{key}='#{value}' #{delimiter} "
+    end
+
+    query = query[0...-(delimiter.length + 1)]
+  end
+
+  def parse_command
+    input = Readline.readline('> ', true)
+    command_input = input.split(' ', 2)
+    command = command_input.first
+
+    parameters = command_input.length == 2 ? command_input.last : ''
+
+    [command, parameters]
+  end
+
+  def execute_command(command, parameters)
+    if @commands.key?(command)
+      @commands[command].call(parameters)
+    else
+      print_help(command)
+    end
   end
 end
 
--- utils.rb
+++ utils.rb
@@ -7,19 +7,24 @@ module ApplicationUtility
       'stats' => 'stats command prints number of users in usersdb.',
       'create' => 'command that create`s new record in database using user`s input.',
       'list' => 'list command shows all records in usersdb.',
-      'edit' => 'edit command allows to edit record in usersdb.',
-      'find' => 'find command allows to search record by given parameter.',
-      'delete' => 'command delete`s one or multiple users by given parameter.'
+      'edit' => 'edit command allows to edit record in usersdb.
+        Params: [<first-name>=value, <last-name>=value, <balance>=value].',
+      'find' => 'find command allows to search record by given parameter.
+        Params: [<first-name>=value, <last-name>=value, <balance>=value].',
+      'delete' => 'command delete`s one or multiple users by given parameter.
+        Params: [<first-name>=value, <last-name>=value, <balance>=value].'
     }
   end
 
-  def self.commands
-    {
-      'help' => method(:print_help),
-      'exit' => method(:exit),
-      'stats' => method(:stats),
-      'create' => method(:create),
-      'list' => method(:list)
-    }
+  def self.parse_selection_params(parameters)
+    args = []
+    parameters.split(' ').each do |value|
+      temp = value.split('=')
+      if (temp.first == 'first-name' || temp.first == 'last-name' || temp.first == 'balance') && temp.last != temp.first
+        args.push({ temp.first => temp.last })
+      end
+    end
+
+    args.reduce Hash.new, :merge
   end
 end
