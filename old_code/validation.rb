38e93e1d827da81a2a6361686a0caa07756a1571
Yauheni Shaikouski <evgenyshaikovskiy@gmail.com>
2022-10-10 13:35:34 +0300

    remove old code

--- old_code/validation.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'extensions'
-
-# module that validates console inputs
-module Validation
-  def self.valid_user?(f_name, l_name, balance)
-    f_name.length > 20 || l_name.length > 20 || !balance.i? || balance.to_i.negative? ? false : true
-  end
-end
